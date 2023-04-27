00b85158c14b3ee4ea7ef682b04b4bf1fe08c409
vitorArgolopc <131806678+vitorArgolopc@users.noreply.github.com>
2023-04-27 13:33:30 -0300

    Adicionados via upload

---
+++ HelloGitHub/src/br/com/vitorargolopc/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.vitorargolopc.git;
+
+/**
+ * @author vitor.argolo
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git");		
+	}
+
+}
